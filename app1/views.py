f7c2dbc83acafbc154e51a580f3371eebe9c88ad
vaishnavm101 <vaishnavm363@gmail.com>
2024-07-11 15:31:04 +0530

    init

---
+++ app1/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    person = {
+        "name": "Yogita",
+        "city": "Pune",
+        "marks": [98, 99, 97, 100]
+    }
+    return render(request, "app1/index.html", person)
